e752a229ba1c01b7bee382a8884df689e60d96f5
AulaMaroon <kadekfp@gmail.com>
2022-01-09 17:05:56 +0800

    Main Commit

---
+++ Language Changer XCOPY.bat
@@ -0,0 +1,10 @@
+@echo off
+start "" "H:\Github\Valorant Language Changer\VALORANT"
+
+:check
+tasklist /nh /fi "imagename eq VALORANT.exe" | findstr /i "VALORANT.exe" >nul && (goto copyfile)|| (echo Waiting For Valorant && goto check)
+
+:copyfile
+xcopy "H:\Github\Valorant Language Changer\Paks" "C:\Riot Games\VALORANT\live\ShooterGame\Content\Paks" /K /H /Y
+
+
---
+++ README.md
@@ -0,0 +1,1 @@
+"# Valorant-Homescreen-Language-Changer" 
